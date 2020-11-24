79c40be36f835fe98ced7ef106ca853494b15c86
Kirill Frolov <k.frolov@samsung.com>
2020-11-24 18:16:54 +0300

    Revert "Fixed build for Windows."
    This reverts commit 49a0048ebd9fd8758555652f996e8b56a6e7e9fc.

--- src/debug/netcoredbg/vscodeprotocol.cpp
+++ src/debug/netcoredbg/vscodeprotocol.cpp
@@ -307,7 +307,7 @@ void VSCodeProtocol::EmitExecEvent(PID pid, const std::string& argv0)
     json body;
 
     body["name"] = argv0;
-    body["systemProcessId"] = DWORD(pid);
+    body["systemProcessId"] = unsigned(pid);
     body["isLocalProcess"] = true;
     body["startMethod"] = "launch";
 
