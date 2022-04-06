78b65f6b73efd9205762ce1c39803c478c0c4384
Alexander Soldatov <soldatov.a@samsung.com>
2022-04-06 22:03:15 +0300

    Bump to 2.0.0-63 version

--- src/version.h
+++ src/version.h
@@ -3,4 +3,4 @@
 // See the LICENSE file in the project root for more information.
 
 // WARNING: do not modify this file! Generated automatically.
-static const char __VERSION[] = "2.0.0-58";
+static const char __VERSION[] = "2.0.0-63";
